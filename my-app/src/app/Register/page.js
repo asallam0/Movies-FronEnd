eefa2c32fa569a25469625458f8976677b9f0027
sallam <asalama656@gmail.com>
2024-10-30 03:46:17 +0300

    Update Register section

--- my-app/src/app/Register/page.js
+++ my-app/src/app/Register/page.js
@@ -46,96 +46,83 @@ const Page=()=>{
         }
     }
 return(
-<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+<div className="flex items-center justify-center min-h-screen bg-[#0f0f0f]">
+      <div className="bg-card p-8 rounded-lg shadow-lg w-full max-w-sm bg-[#191919]">
+        <div className="flex items-center justify-center mb-4">
           <img
-            alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
+            aria-hidden="true"
+            alt="logo"
+            src="https://openui.fly.dev/openui/24x24.svg?text=🔵"
+            className="w-12 h-12"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
         </div>
+        <h2 className="text-2xl text-[#fbfbfb] font-bold text-foreground text-center  mb-2">Welcome!</h2>
+       
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form on onSubmit={registraFun}>
-            <div>
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-               Name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  value={registra.name}
-                  onChange={(e) => setregistra({ ...registra, name: e.target.value })}
-                  required
-                  autoComplete="name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  value={registra.email}
-                  onChange={(e) => setregistra({ ...registra, email: e.target.value })}
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  value={registra.password}
-                  onChange={(e) => setregistra({ ...registra, password: e.target.value })}
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign up
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            <Link href={"/Login"}>Sign in</Link>
+        <form on onSubmit={registraFun}>
+          <div className="mb-4">
+            <label htmlFor="name" className="block text-[#fbfbfb] text-muted-foreground mb-1"> Name</label>
             
-          </p>
+            <input
+              type="name"
+              id="name"
+              value={registra.name}
+               className="w-full bg-[#121212]  text-white p-2 rounded-lg focus:outline-none focus:ring focus:ring-ring"
+              placeholder="Name"
+              onChange={(e) => setregistra({ ...registra, name: e.target.value })}
+               autoComplete="name"
+              required
+
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-[#fbfbfb] text-muted-foreground mb-1">Email Address</label>
+            
+            <input
+              type="email"
+              id="email"
+              value={registra.email}
+               className="w-full bg-[#121212]  text-white p-2 rounded-lg focus:outline-none focus:ring focus:ring-ring"
+              placeholder="email address"
+              onChange={(e) => setregistra({ ...registra, email: e.target.value })}
+               autoComplete="email"
+              required
+
+            />
+          </div>
+          <div className="mb-4 ">
+            <label htmlFor="password" className="block text-[#fbfbfb] text-muted-foreground mb-1">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={registra.password}
+                  onChange={(e) => setregistra({ ...registra, password: e.target.value })}
+             autoComplete="current-password"
+              className="w-full bg-[#121212]   text-white p-2  rounded-lg focus:outline-none focus:ring focus:ring-ring"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full text-[#fbfbfb] bg-primary text-primary-foreground p-2 rounded-lg hover:bg-primary/80"
+          >
+            Register
+          </button>
+        </form>
+
+        <div className="flex items-center justify-between my-4">
+          <hr className="flex-grow border-muted" />
+          <span className="text-muted-foreground mx-2">OR</span>
+          <hr className="flex-grow border-muted" />
         </div>
+
+        <p className="text-muted-foreground text-[#fbfbfb] text-center mb-6">
+         If You have an account?{' '}
+          <Link  href={"/Login"} className="text-primary hover:underline text-[#fbfbfb]">Sign in</Link>
+        </p>
       </div>
+    </div>
     )
 }
 export default Page;
